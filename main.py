4d699a8a706c5741058335d081f504ce85d91f36
hallie <hhpham218@gmail.com>
2022-11-19 18:46:55 -0800

    initial start

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# test
+#  small dataset 46 & large dataset 78
+import random
+
+
+#  leave one out cross validation stub code
+def k_fold_cross_validation(data, current_set, feature_to_add):
+    rand = round(random.random(), 3)
+    return rand
+
+
+def feature_search(data):
+    for x in len(data):
+        print('On the ', x, 'th level of the search tree')
+
