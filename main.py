3869ed9489ac4dbc8db63d1824887f02b9f521d0
hallie <hhpham218@gmail.com>
2022-12-03 15:09:51 -0800

    backup b4 i try smthng CRAZY!

--- main.py
+++ main.py
@@ -4,38 +4,46 @@ import pandas as pd
 import math
 
 df = pd.read_table('CS170_Small_Data__46.txt', engine='python', delimiter='  ',
-                   names=['class label', 'feature 1', 'feature 2', 'feature 3', 'feature 4', 'feature 5', 'feature 6'])
-small_test_df = df.head(10)
+                   names=['class label', '1', '2', '3', '4', '5', '6'])
+test_df = pd.read_table('CS170_Small_Data__6.txt', engine='python', delimiter='  ',
+                        names=['class label', '1', '2', '3', '4', '5', '6'])
+small_test_df = df.head(20)
 
 
 #  leave one out cross validation stub code
-def k_fold_cross_validation(data, current_set, feature_to_add):
-    rand = round(random.random(), 3)
-    return rand
+def k_fold_cross_validation(data, current_set, feature_to_add):  # returns accuracy
+    # code to do k folding goes here
+    features = []
+    for x in current_set:
+        features.append(str(x))
+    features.append(str(feature_to_add))
+    data_shaped = data[features]
 
-
-def test_accuracy(data):
+    # classification accuracy
     count_of_correctly_classified = 0
     for i in range(len(data)):
-        object_to_classify = data.iloc[i, 1:7]  # features
+        object_to_classify = data.iloc[i, 1:]  # list of features at index i
         # print(object_to_classify)
-        label_object_to_classify = data.iloc[i, 0]  # class label
+        label_object_to_classify = data['class label'].iloc[i]  # class label at index i
 
         nearest_neighbor_distance = 10000
         nearest_neighbor_location = 10000
         nearest_neighbor_label = 0
         for k in range(len(data.columns)-1):
             if k is not i:
-                print('Ask if', str(i), 'is nearest neighbor with', str(k))
-                distance = math.sqrt(sum((object_to_classify[k]-data.iloc[k])**2))  # one feature or all of them ?
+                # print('Ask if', str(i), 'is nearest neighbor with', str(k))
+                # print('object to classify', object_to_classify[k])
+                # print('data shaped:', data.iloc[k])
+                distance = math.sqrt(sum((object_to_classify-data.iloc[k, 1:])**2))  # one feature or all of them ?
+                # print(distance)
                 if distance < nearest_neighbor_distance:
                     nearest_neighbor_distance = distance
                     nearest_neighbor_location = k
-                    nearest_neighbor_label = data.iloc[nearest_neighbor_location, 0]
+                    nearest_neighbor_label = data['class label'].iloc[nearest_neighbor_location]
+        # print('Object', str(i), 'is class', str(label_object_to_classify))
+        # print('Its nearest neighbor is', str(nearest_neighbor_location), 'which is in class', str(nearest_neighbor_label))
         if label_object_to_classify == nearest_neighbor_label:
             count_of_correctly_classified += 1
-    print('Object', str(i), 'is class', str(label_object_to_classify))
-    print('Its nearest neighbor is', str(nearest_neighbor_location), 'which is in class', str(nearest_neighbor_label))
     accuracy = count_of_correctly_classified/len(data)
     print(accuracy)
     return accuracy
@@ -61,11 +69,6 @@ def feature_search(data):
         print('On level', str(x), 'I added feature', str(feature_to_add_at_this_level), 'to current set')
 
 
-"""print(df)
-df1 = df.head(5)
-print(df1)
-feature_search(df1)"""
-test_accuracy(small_test_df)
-#hi = small_test_df.iloc[0, 1:7]
-#print(small_test_df.iloc[0, 1:7])
+k_fold_cross_validation(test_df, [2, 3], 5)
+# print(small_test_df[['class label','2', '3']])
 
