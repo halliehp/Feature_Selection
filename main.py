f66afc41c3d7a927bba546cc6679cbb967c7eead
hallie <hhpham218@gmail.com>
2022-12-03 16:24:41 -0800

    slightly working but not super accurately

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #  small dataset 46 & large dataset 78
-import random
 import pandas as pd
 import math
 
@@ -13,12 +12,12 @@ small_test_df = df.head(20)
 #  leave one out cross validation stub code
 def k_fold_cross_validation(data, current_set, feature_to_add):  # returns accuracy
     # code to do k folding goes here
-    features = []
+    features = ['class label']
     for x in current_set:
         features.append(str(x))
     features.append(str(feature_to_add))
-    data_shaped = data[features]
-
+    data = data[features]
+    print(data)
     # classification accuracy
     count_of_correctly_classified = 0
     for i in range(len(data)):
@@ -27,23 +26,24 @@ def k_fold_cross_validation(data, current_set, feature_to_add):  # returns accur
         label_object_to_classify = data['class label'].iloc[i]  # class label at index i
 
         nearest_neighbor_distance = 10000
-        nearest_neighbor_location = 10000
         nearest_neighbor_label = 0
-        for k in range(len(data.columns)-1):
+        for k in range(len(data)):
             if k is not i:
                 # print('Ask if', str(i), 'is nearest neighbor with', str(k))
                 # print('object to classify', object_to_classify[k])
                 # print('data shaped:', data.iloc[k])
-                distance = math.sqrt(sum((object_to_classify-data.iloc[k, 1:])**2))  # one feature or all of them ?
+
+                distance = math.sqrt(sum((object_to_classify - data.iloc[k, 1:])**2))
+                # distance = math.sqrt(sum(object_to_classify**2))
                 # print(distance)
                 if distance < nearest_neighbor_distance:
                     nearest_neighbor_distance = distance
                     nearest_neighbor_location = k
                     nearest_neighbor_label = data['class label'].iloc[nearest_neighbor_location]
-        # print('Object', str(i), 'is class', str(label_object_to_classify))
-        # print('Its nearest neighbor is', str(nearest_neighbor_location), 'which is in class', str(nearest_neighbor_label))
         if label_object_to_classify == nearest_neighbor_label:
             count_of_correctly_classified += 1
+        # print('Object', str(i), 'is class', str(label_object_to_classify))
+        # print('Its nearest neighbor is', str(nearest_neighbor_location), 'which is in class', str(nearest_neighbor_label))
     accuracy = count_of_correctly_classified/len(data)
     print(accuracy)
     return accuracy
@@ -69,6 +69,6 @@ def feature_search(data):
         print('On level', str(x), 'I added feature', str(feature_to_add_at_this_level), 'to current set')
 
 
-k_fold_cross_validation(test_df, [2, 3], 5)
+k_fold_cross_validation(test_df, [2, 5], 3)
 # print(small_test_df[['class label','2', '3']])
 
