d0b3444b387d13e2b6d4857c3c600e28b03af504
hallie <hhpham218@gmail.com>
2022-12-08 00:08:08 -0800

    feature search working! tested on sample sets 96 & 88

--- main.py
+++ main.py
@@ -2,24 +2,24 @@
 import pandas as pd
 import math
 import numpy as np
+import copy
 
 df = pd.read_table('CS170_Small_Data__46.txt', engine='python', delimiter='  ',
                    names=['class label', '1', '2', '3', '4', '5', '6'])
-test_df = pd.read_table('CS170_Small_Data__96.txt', engine='python', delimiter='  ',
+test_df = pd.read_table('CS170_Small_Data__88.txt', engine='python', delimiter='  ',
                         names=['class label', '1', '2', '3', '4', '5', '6'])
 small_test_df = df.head(20)
 arr = test_df.to_numpy()
 
 
-def dist(a, b):
-    # cum = a[:] - b[:]
-    cum = a - b
-    return np.dot(cum, cum)
+def calculate_distance(a, b):
+    sub = a - b
+    return np.dot(sub, sub)
 
 
 def k_fold_cross_validation_anti_pandas(data, current_set, feature_to_add):  # returns accuracy
     # code to do k folding goes here
-    features = current_set
+    features = copy.deepcopy(current_set)
     features.append(feature_to_add)
 
     count_of_correctly_classified = 0
@@ -37,9 +37,7 @@ def k_fold_cross_validation_anti_pandas(data, current_set, feature_to_add):  # r
                 for s in range(len(features)):
                     temp.append(data[k][features[s]])  # list of features at index k
                 temp = np.array(temp)
-                # distance = np.sqrt(sum(np.subtract(object_to_classify, temp) ** 2))
-                # distance = np.dot(object_to_classify, temp)
-                distance = dist(object_to_classify, temp)
+                distance = calculate_distance(object_to_classify, temp)
                 if distance < nearest_neighbor_distance:
                     nearest_neighbor_distance = distance
                     nearest_neighbor_location = k
@@ -47,8 +45,8 @@ def k_fold_cross_validation_anti_pandas(data, current_set, feature_to_add):  # r
         if label_object_to_classify == nearest_neighbor_label:
             count_of_correctly_classified += 1
     accuracy = count_of_correctly_classified/len(data)
-    print('correctly classified out of 500:', count_of_correctly_classified)
-    print(accuracy)
+    # print('correctly classified out of 500:', count_of_correctly_classified)
+    # print(accuracy)
     return accuracy
 
 
@@ -91,23 +89,23 @@ def feature_search(data):
     # forward selection
     for x in range(len(data.columns)-1):
         print('On the', x, 'th level of the search tree')
+        feature_to_add_at_this_level = []
         best_so_far_accuracy = 0
-        accuracy = 0
 
         for k in range(len(data.columns)-1):  # make sure to not count the class label column
-            if k not in current_set_of_features:
+            if k+1 not in current_set_of_features:
                 # current_set_of_features.append(k)
                 pls = data.to_numpy()
                 accuracy = k_fold_cross_validation_anti_pandas(pls, current_set_of_features, k+1)
-                print('---Using features', str(current_set_of_features), str(k), 'the accuracy is', accuracy)
-            if accuracy > best_so_far_accuracy:
-                best_so_far_accuracy = accuracy
-                feature_to_add_at_this_level = k+1
-        current_set_of_features.append(feature_to_add_at_this_level)
+                print('---Using features', str(current_set_of_features), str(k+1), 'the accuracy is', accuracy)
+                if accuracy > best_so_far_accuracy:
+                    best_so_far_accuracy = accuracy
+                    feature_to_add_at_this_level = k+1
         print('On level', str(x), 'I added feature', str(feature_to_add_at_this_level), 'to current set')
+        current_set_of_features.append(feature_to_add_at_this_level)
         if best_so_far_accuracy > best_accuracy:
             best_accuracy = best_so_far_accuracy
-            best_features = current_set_of_features
+            best_features = copy.deepcopy(current_set_of_features)
     print('best accuracy:', best_accuracy)
     print('best features:', best_features)
     return best_features
