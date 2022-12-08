4aa9f533a66d50932ce29691d16acd04bf24f377
hallie <hhpham218@gmail.com>
2022-12-08 00:08:43 -0800

    feature search working! tested on sample sets 96 & 88

--- main.py
+++ main.py
@@ -50,38 +50,6 @@ def k_fold_cross_validation_anti_pandas(data, current_set, feature_to_add):  # r
     return accuracy
 
 
-#  leave one out cross validation stub code
-def k_fold_cross_validation(data, current_set, feature_to_add):  # returns accuracy
-    # code to do k folding goes here
-    features = ['class label']
-    for x in current_set:
-        features.append(str(x))
-    features.append(str(feature_to_add))
-    data = data[features]
-    # print(data)
-    # classification accuracy
-    count_of_correctly_classified = 0
-    for i in range(len(data)):
-        object_to_classify = data.iloc[i, 1:]  # list of features at index i
-        label_object_to_classify = data['class label'].iloc[i]  # class label at index i
-
-        nearest_neighbor_distance = math.inf
-        nearest_neighbor_label = 0
-        for k in range(len(data)):
-            if k is not i:
-                distance = math.sqrt(sum((object_to_classify - data.iloc[k, 1:]) ** 2))
-                if distance < nearest_neighbor_distance:
-                    nearest_neighbor_distance = distance
-                    nearest_neighbor_location = k
-                    nearest_neighbor_label = data['class label'].iloc[nearest_neighbor_location]
-        if label_object_to_classify == nearest_neighbor_label and nearest_neighbor_label != 0:
-            count_of_correctly_classified += 1
-    accuracy = count_of_correctly_classified/len(data)
-    # print('correctly classified out of 500:', count_of_correctly_classified)
-    print(accuracy)
-    return accuracy
-
-
 def feature_search(data):
     current_set_of_features = []
     best_accuracy = 0
